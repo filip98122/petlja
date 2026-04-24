4d8addf13bc61ac315ed65e46f7887ba69c59cf9
Filip Markovic <filip98122@gmail.com>
2026-04-24 14:16:25 +0200

    LUKA!!!

--- rotiranje reci.py
+++ rotiranje reci.py
@@ -1,19 +1,28 @@
 import math
 l=list(map(str,(input().strip()).split()))
 for i in range(len(l)):
-    zapamti=l[i]
-    res=0
+    zapamti=l[i] # Current original word
+    res=0 #kolko se do sada promenilo
     tren=l[i]
-    for j in range(len(l[i])):
-        if i>len(l[i])//2:
-            res=len(l[i])
-            break
-        k=tren[0]
-        tren=tren[1:]
-        tren=tren+k
-        res+=1
-        if tren==zapamti:
-            break
-    l[i]=res
+    nemoj=False
+    
+    new = zapamti+zapamti
+    #for j in range(len(l[i])//2+1):
+    #    # Shift
+    #    k=tren[0]
+    #    tren=tren[1:]
+    #    tren=tren+k
+    #    res+=1
+    #    
+    #    if tren==zapamti:
+    #        nemoj=True
+    #        break
+    #if not nemoj:
+    #    res=len(l[i])
+    #l[i]=res
+    l[i] = (new+new).find(new,1)
+    
+    #abababab
+    
     
 print(math.lcm(*l))
